4cdc721cbeec155a1414556a47e4d6839cbe7bf5
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2023-07-21 21:01:33 +0200

    Update LeapYear.swift

--- Katas_1/Katas/LeapYear.swift
+++ Katas_1/Katas/LeapYear.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+func LeapYear(year: Int) -> String {
+    if year % 400 == 0 {
+        return "Leap year."
+    }
+    else if year % 400 != 0 && year % 100 == 0 {
+        return "Not leap year."
+    }
+    else if year % 100 != 0 && year % 4 == 0 {
+        return "Leap year."
+    }
+    else if year % 4 != 0 {
+        return "Not leap year."
+    }
+    return "1"
+}
