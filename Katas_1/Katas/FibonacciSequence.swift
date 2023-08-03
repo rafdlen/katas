0d7245f645c455af764d3f4b17bc892f9a47f43d
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2023-08-03 17:27:27 +0200

    Add files via upload

--- Katas_1/Katas/FibonacciSequence.swift
+++ Katas_1/Katas/FibonacciSequence.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
-func FibonacciSequence(num: Int) -> Int {
-    if num == 0 {
-        return 0
+struct Fibonacci {
+    func calculate(_ position: Int) -> Int
+    {
+        if position == 1 {
+            return 0
+        }
+        if position == 2 {
+            return 1
+        }
+        if position == 3 {
+            return 1
+        }
+        return self.calculate(position - 2) + self.calculate(position - 1)
+        }
     }
-    else if num == 1 {
-        return 1
-    }
-    else {
-        return FibonacciSequence(num: num - 1) + FibonacciSequence(num: num - 2)
-    }
-}
+
